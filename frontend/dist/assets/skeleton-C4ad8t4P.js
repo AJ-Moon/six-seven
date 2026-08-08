bbadd79d56fa00c0f256d587777ef7cce91b545a
AJ-Moon <146750397+AJ-Moon@users.noreply.github.com>
2026-08-08 17:03:58 +0500

    Add files via upload

---
+++ frontend/dist/assets/skeleton-C4ad8t4P.js
@@ -0,0 +1,1 @@
+import{n as e}from"./createLucideIcon-Bo4p3jxk.js";import{t}from"./utils-DXalBF5w.js";var n=e();function r({className:e,...r}){return(0,n.jsx)(`div`,{"data-slot":`skeleton`,className:t(`bg-accent animate-pulse rounded-md`,e),...r})}export{r as t};
